89bbb37e0b744fe2ef0fc86a4349773d293fc5f7
Thipyarat9 <thipyarat.se.65@ubu.ac.th>
2023-06-28 21:25:13 +0700

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Footer() {
+    return (
+        <div className='MyFooter'>
+            <span>2023 @Artitaya Senkamson</span>
+        </div>
+    );
+}
+
+export default Footer;
